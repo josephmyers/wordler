86a0641d6385a78540b45cb683e9a4f489c6b948
josephmyers <wjosephmyers@gmail.com>
2023-03-01 13:52:40 +0530

    Replaced help text with groundwork for Help/About.
    Help/About will gently flash when we detect the user may need help. This can be stopped by either setting a status or clicking the icon. In the future, the latter will open a model dialog with the Help/About info.
    
    This overall seems more flexible and more professional than showing some simple text in the header. It also gives me a chance to put my info on the site.

--- src/HelpText.js
+++ src/HelpText.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './HelpText.css';
+import question from './question.svg';
 
 export default function HelpText(props) {
-  let text;
-  const [shouldDisplay, setShouldDisplay] = React.useState(false);
   const [hasSetStatus, setHasSetStatus] = React.useState(false);
   const [secondsLeft, setSecondsLeft] =  React.useState(2);
-  const [fade, setFade] = React.useState('fade-out');
+  const [fade, setFade] = React.useState('fade-in');
+  const [flashHelp, setFlashHelp] = React.useState(false);
+  const [helpOpened, setHelpOpened] = React.useState(false);
 
   React.useEffect(() => {
     const timeout = setInterval(() => {
-      if (shouldDisplay)
+      if (!hasSetStatus && secondsLeft === 0 && flashHelp)
       {
         if (fade === 'fade-in') {
           setFade('fade-out');
@@ -18,23 +19,23 @@ export default function HelpText(props) {
           setFade('fade-in');
         }
       }
-    }, 2000);
+      else if (hasSetStatus || !flashHelp)
+      {
+        setFade('fade-in');
+      }
+    }, 1000);
     return () => clearInterval(timeout);
-  }, [fade, shouldDisplay]);
+  }, [fade, secondsLeft, hasSetStatus, flashHelp]);
 
   React.useEffect(()=> {
     let myInterval = setInterval(() =>
       {
         if (secondsLeft > 0 && props.words.length > 2) {
           setSecondsLeft(secondsLeft - 1);
-        }
-        if (secondsLeft === 0 && !hasSetStatus)
-        {
-          setShouldDisplay(true);
-        }
-        else
-        {
-          setShouldDisplay(false);
+          if (!helpOpened)
+          {
+            setFlashHelp(true);
+          }
         }
       }, 1000);
     return () => clearInterval(myInterval);
@@ -42,19 +43,25 @@ export default function HelpText(props) {
 
   if (!hasSetStatus)
   {
-    text = "Touch letter to filter";
     const words = props.words;
     for (let w in words)
     {
       if (words[w].letters.filter(l => l.status !== 0).length > 0)
       {
         setHasSetStatus(true);
-        return;
+        break;
       }
     }
-  
-    return (shouldDisplay && <div className={fade}>{text}</div>);
   }
 
-  return;
+  function showHelp() {
+    setFlashHelp(false);
+    setHelpOpened(true);
+  }
+
+  return (
+    <div className={fade} onClick={showHelp}>
+      <img src={question} className="icon" alt="help" />
+    </div>
+  );
 }
